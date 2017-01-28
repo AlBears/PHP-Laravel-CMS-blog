a9cef6e8478562f79acc52772d7ef5122947f337
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-01-28 11:56:22 +0300

    display posts, image_url

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-Route::get('/', function () {
-    return view('blog.index');
-});
+Route::get('/', [
+  'uses' => 'BlogController@index'
+]);
 
 Route::get('/blog/show', function () {
     return view('blog.show');
