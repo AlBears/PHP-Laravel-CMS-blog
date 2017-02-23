2dc9db715e53878465461f41363e44990f331945
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-02-23 10:39:51 +0300

    add new blog form

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
 
          GrahamCampbell\Markdown\MarkdownServiceProvider::class,
+         Collective\Html\HtmlServiceProvider::class,
 
         //
 
@@ -228,6 +229,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
