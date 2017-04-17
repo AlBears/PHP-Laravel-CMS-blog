034e08bc16f303374c588025e4e74d1bd93c36e3
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-04-17 11:28:41 +0300

    image thumbnail

--- config/app.php
+++ config/app.php
@@ -163,13 +163,14 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
 
          GrahamCampbell\Markdown\MarkdownServiceProvider::class,
          Collective\Html\HtmlServiceProvider::class,
-
+         Intervention\Image\ImageServiceProvider::class,
         //
 
         /*
@@ -231,6 +232,7 @@ return [
         'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
