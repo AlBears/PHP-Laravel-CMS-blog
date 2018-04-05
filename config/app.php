ee0fe2806f8f1a7e9e954b955330fe3763cd82b0
Alexey Sidorovitch <sidorovitch@gmail.com>
2018-04-05 18:02:06 +0300

    roles

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        
 
 
         /*
@@ -171,6 +172,7 @@ return [
          GrahamCampbell\Markdown\MarkdownServiceProvider::class,
          Collective\Html\HtmlServiceProvider::class,
          Intervention\Image\ImageServiceProvider::class,
+         Laratrust\LaratrustServiceProvider::class,
         //
 
         /*
@@ -233,6 +235,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'Laratrust'   => Laratrust\LaratrustFacade::class,
 
     ],
 
